054514ce513ea7cad15c5c651f5d054ec050807d
Stefan <dev@saru.id>
2026-08-10 22:00:02 +0200

    jumon formula

---
+++ Formula/jumon.rb
@@ -0,0 +1,27 @@
+class Jumon < Formula
+  desc "Incantations for your shell: say it, get the command, you run it"
+  homepage "https://github.com/saru-id/jumon"
+  version "1.2.0"
+  url "https://github.com/saru-id/jumon/releases/download/v1.2.0/jumon-v1.2.0-aarch64-apple-darwin.tar.gz"
+  sha256 "PLACEHOLDER"
+
+  depends_on arch: :arm64
+  depends_on :macos
+
+  def install
+    bin.install "jumon"
+    man1.install Dir["share/man/man1/*.1"]
+    zsh_completion.install "share/zsh/site-functions/_jumon"
+  end
+
+  def caveats
+    <<~EOS
+      Finish setup (config, widget, and your .zshrc line):
+        jumon init
+    EOS
+  end
+
+  test do
+    assert_match "jumon", shell_output("#{bin}/jumon --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# saru-id/homebrew-tap
+
+```sh
+brew install saru-id/tap/jumon
+```
+
+Formulae for saru-id tools. Note: `jumon` installs from its GitHub
+release tarball; until that repository is public, the download needs
+access to it.
