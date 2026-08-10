04bf68370fcc40d2be616d3b888d6b86b4cd55bc
Stefan <dev@saru.id>
2026-08-10 22:04:12 +0200

    jumon v1.2.0 sha

--- Formula/jumon.rb
+++ Formula/jumon.rb
@@ -3,7 +3,7 @@ class Jumon < Formula
   homepage "https://github.com/saru-id/jumon"
   version "1.2.0"
   url "https://github.com/saru-id/jumon/releases/download/v1.2.0/jumon-v1.2.0-aarch64-apple-darwin.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "e0943e979634f57248ac0db7cbfb9e813dbe69ba5227fad1f7bfdea3d3d1fa72"
 
   depends_on arch: :arm64
   depends_on :macos
