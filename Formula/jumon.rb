75dfd4c927f2cdd76a95724a12ee44e24fa4c7bd
Stefan <dev@saru.id>
2026-08-11 02:06:04 +0200

    jumon 1.13.2

--- Formula/jumon.rb
+++ Formula/jumon.rb
@@ -1,9 +1,9 @@
 class Jumon < Formula
   desc "Incantations for your shell: say it, get the command, you run it"
   homepage "https://github.com/saru-id/jumon"
-  version "1.13.1"
-  url "https://github.com/saru-id/jumon/releases/download/v1.13.1/jumon-v1.13.1-aarch64-apple-darwin.tar.gz"
-  sha256 "b13fdff2f905e851bf0cff79f97c8e143ae8d0ff94520b08fdca303e2983cecd"
+  version "1.13.2"
+  url "https://github.com/saru-id/jumon/releases/download/v1.13.2/jumon-v1.13.2-aarch64-apple-darwin.tar.gz"
+  sha256 "bd7924bc97a0fbc578abcda944b8e1f4c3a90dd8b65f977e718f01c9c5b93525"
 
   depends_on arch: :arm64
   depends_on :macos
